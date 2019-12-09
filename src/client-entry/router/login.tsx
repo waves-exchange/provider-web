2c3ce71e9ff8f5c51f0769d12a30b60aad785990
dtsigelnitskiy <dtsigelnitskiy@wv00505.local>
2019-12-09 17:55:43 +0300

    Add linters

--- src/client-entry/router/login.tsx
+++ src/client-entry/router/login.tsx
@@ -4,35 +4,36 @@ import renderPage from '../utils/renderPage';
 import { libs } from '@waves/waves-transactions';
 import { IUserData } from '@waves/waves-js/src/interface';
 import { hasUsers } from '../services/userService';
-import React = require('react');
 import CreateAccount from '../pages/login/CreateAccount';
 
+import React = require('react');
 
-export default function (state: IState) {
+export default function(state: IState) {
     return (): Promise<IUserData> => {
-        if (state.user) {
+        if (state.user != null) {
             return Promise.resolve({
                 address: state.user.address,
-                publicKey: libs.crypto.publicKey(state.user.seed)
+                publicKey: libs.crypto.publicKey(state.user.seed),
             });
         } else {
-
             const Page = hasUsers() ? Login : CreateAccount;
 
             return new Promise((resolve, reject) => {
-                renderPage(<Page
-                    networkByte={state.networkByte}
-                    onCancel={() => {
-                        reject('User rejection!');
-                    }}
-                    onConfirm={user => {
-                        state.user = user;
-                        resolve({
-                            address: user.address,
-                            publicKey: libs.crypto.publicKey(user.seed)
-                        });
-                    }}
-                />);
+                renderPage(
+                    <Page
+                        networkByte={state.networkByte}
+                        onCancel={() => {
+                            reject('User rejection!');
+                        }}
+                        onConfirm={(user) => {
+                            state.user = user;
+                            resolve({
+                                address: user.address,
+                                publicKey: libs.crypto.publicKey(user.seed),
+                            });
+                        }}
+                    />
+                );
             });
         }
     };
