05d4cb9a340ea8f548bda69d6049313c65e5a7e3
Viktor Voronin <vvoronin@web3tech.ru>
2019-12-20 17:53:00 +0300

    Refactor invoke

---
+++ src/client-entry/components/InvokeFunction/helpers.ts
@@ -0,0 +1,34 @@
+import { CSSObject } from '@styled-system/css';
+import { TInvokeScriptCallArgument } from '@waves/ts-types';
+import { TLong } from '@waves/waves-js/dist/src/interface';
+import { pseudoElemStyles, COLOR_MAP } from './styles';
+
+export const getAttrStyles = (
+    attrType: TInvokeScriptCallArgument<TLong>['type'],
+    isLast: boolean
+): CSSObject => ({
+    variant: 'body2',
+    fontFamily: 'Menlo-Regular',
+    color: COLOR_MAP[attrType],
+    ':after': !isLast ? { ...pseudoElemStyles, content: '", "' } : {},
+});
+
+const formatText = (text: string): string =>
+    text.length > 5 ? `${text.slice(0, 4)}...` : text;
+
+export const getAttrContent = (
+    type: TInvokeScriptCallArgument<TLong>['type'],
+    value: TInvokeScriptCallArgument<TLong>['value']
+): string | null => {
+    switch (type) {
+        case 'integer':
+        case 'boolean':
+            return String(value);
+        case 'string':
+            return `'${formatText(value as string)}'`;
+        case 'binary':
+            return 'base64:...';
+        default:
+            return null;
+    }
+};
