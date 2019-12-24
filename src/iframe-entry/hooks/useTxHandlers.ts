8e664e8f82c70e11445fda9f56b34f42e3095d95
Viktor Voronin <vvoronin@web3tech.ru>
2019-12-24 12:27:30 +0300

    DEXW-2629: add transaction handlers hook

---
+++ src/iframe-entry/hooks/useTxHandlers.ts
@@ -0,0 +1,46 @@
+import { MouseEventHandler, useCallback } from 'react';
+import { analytics, IEventData } from '../../client-entry/utils/analytics';
+import { IWithId, TTransaction } from '@waves/ts-types';
+import { TLong } from '@waves/waves-js';
+
+type AnalyticsArgs = {
+    onRejectAnalyticsArgs: IEventData;
+    onConfirmAnalyticsArgs: IEventData;
+};
+
+type TxHandlers = {
+    handleReject: MouseEventHandler<HTMLButtonElement>;
+    handleConfirm: MouseEventHandler<HTMLButtonElement>;
+};
+
+type UseTxHandlers = (
+    tx: TTransaction<TLong> & IWithId,
+    onReject: () => void,
+    onConfirm: (tx: TTransaction<TLong>) => void,
+    analyticsArgs: AnalyticsArgs
+) => TxHandlers;
+
+export const useTxHandlers: UseTxHandlers = (
+    tx,
+    onReject,
+    onConfirm,
+    { onRejectAnalyticsArgs, onConfirmAnalyticsArgs }
+) => {
+    const handleReject = useCallback<
+        MouseEventHandler<HTMLButtonElement>
+    >(() => {
+        onReject();
+
+        analytics.send(onRejectAnalyticsArgs);
+    }, [onReject, onRejectAnalyticsArgs]);
+
+    const handleConfirm = useCallback<
+        MouseEventHandler<HTMLButtonElement>
+    >(() => {
+        onConfirm(tx);
+
+        analytics.send(onConfirmAnalyticsArgs);
+    }, [onConfirm, onConfirmAnalyticsArgs, tx]);
+
+    return { handleConfirm, handleReject };
+};
