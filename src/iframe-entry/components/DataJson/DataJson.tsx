bc6d2c7904fdbfc8f69a2d5c198dc8261adcf1b5
okanishcheva <okanishcheva@wavesplatform.com>
2020-01-28 11:39:01 +0300

    DEXW-2666: add tabs

---
+++ src/iframe-entry/components/DataJson/DataJson.tsx
@@ -0,0 +1,26 @@
+import { Box, Text } from '@waves.exchange/react-uikit';
+import React, { FC } from 'react';
+
+type DataJsonProps = {
+    data: any;
+};
+
+export const DataJson: FC<DataJsonProps> = ({ data }) => (
+    <Box
+        borderRadius="$4"
+        bg="basic.$900"
+        p="15px"
+        maxHeight="165px"
+        overflowY="auto"
+        color="standard.$0"
+    >
+        <Text
+            as="pre"
+            m="0"
+            variant="body2"
+            fontFamily="Menlo, Monaco, Consolas, Courier New, monospace"
+        >
+            {JSON.stringify(data, null, 2)}
+        </Text>
+    </Box>
+);
