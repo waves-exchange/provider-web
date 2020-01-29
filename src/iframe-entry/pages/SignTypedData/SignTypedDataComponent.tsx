052abed9ea23cf280ee205f3a297e5cd767764a8
finico <agonchar@web3tech.ru>
2020-01-29 19:03:32 +0300

    DEXW-2702: Fix design (TypedData)

--- src/iframe-entry/pages/SignTypedData/SignTypedDataComponent.tsx
+++ src/iframe-entry/pages/SignTypedData/SignTypedDataComponent.tsx
@@ -31,67 +31,59 @@ export const SignTypedDataComponent: FC<SignTypedDataComponentProps> = ({
     data,
     onReject,
     onConfirm,
-}) => {
-    return (
-        <Confirmation
-            address={userAddress}
-            name={userName}
-            balance={userBalance}
-            onReject={onReject}
-            onConfirm={onConfirm}
-        >
-            <Flex px="$40" py="$20" bg="main.$900">
-                <Flex
-                    alignItems="center"
-                    justifyContent="center"
-                    borderRadius="circle"
-                    bg="rgba(255, 175, 0, 0.1)"
-                    height={60}
-                    width={60}
-                >
-                    <Icon
-                        size="60px"
-                        icon={iconSignMessage}
-                        color="primary.$300"
-                    />
-                </Flex>
-                <Flex ml="$20" flexDirection="column">
-                    <Text variant="body1" color="basic.$500">
-                        Off-chain
-                    </Text>
-                    <Text fontSize={26} lineHeight="32px" color="standard.$0">
-                        Sign Data
-                    </Text>
-                </Flex>
+}) => (
+    <Confirmation
+        address={userAddress}
+        name={userName}
+        balance={userBalance}
+        onReject={onReject}
+        onConfirm={onConfirm}
+    >
+        <Flex px="$40" py="$20" bg="main.$900">
+            <Flex
+                alignItems="center"
+                justifyContent="center"
+                borderRadius="circle"
+                bg="rgba(255, 175, 0, 0.1)"
+                height={60}
+                width={60}
+            >
+                <Icon size="60px" icon={iconSignMessage} color="primary.$300" />
             </Flex>
+            <Flex ml="$20" flexDirection="column">
+                <Text variant="body1" color="basic.$500">
+                    Off-chain
+                </Text>
+                <Text fontSize={26} lineHeight="32px" color="standard.$0">
+                    Sign Data
+                </Text>
+            </Flex>
+        </Flex>
 
-            <Tabs>
-                <TabsList
-                    borderBottom="1px solid"
-                    borderColor="main.$700"
-                    mb="$30"
-                    px="$40"
-                    bg="main.$900"
-                >
-                    <Tab mr="32px" pb="12px">
-                        <Text variant="body1">Data</Text>
-                    </Tab>
-                    <Tab mr="32px" pb="12px">
-                        <Text variant="body1">JSON</Text>
-                    </Tab>
-                </TabsList>
+        <Tabs>
+            <TabsList
+                borderBottom="1px solid"
+                borderColor="main.$700"
+                mb="$30"
+                px="$40"
+                bg="main.$900"
+            >
+                <Tab mr="32px" pb="12px">
+                    <Text variant="body1">Data</Text>
+                </Tab>
+                <Tab mr="32px" pb="12px">
+                    <Text variant="body1">JSON</Text>
+                </Tab>
+            </TabsList>
 
-                <Flex flexDirection="column" mb="$30" px="$40" bg="main.$800">
-                    <TabPanels>
-                        <TabPanel>
-                            <DataEntry data={data} />
-                        </TabPanel>
-                        <TabPanel>
-                            <DataJson data={data} />
-                        </TabPanel>
-                    </TabPanels>
-                </Flex>
-            </Tabs>
-        </Confirmation>
-    );
-};
+            <TabPanels mb="$30" px="$40" bg="main.$800">
+                <TabPanel>
+                    <DataEntry data={data} />
+                </TabPanel>
+                <TabPanel>
+                    <DataJson data={data} />
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+    </Confirmation>
+);
