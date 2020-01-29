e6a116f841f68f645a05cb6b1bf76f8a750590f0
finico <agonchar@web3tech.ru>
2020-01-29 19:03:14 +0300

    DEXW-2702: Fix design (Transfer)

--- src/iframe-entry/pages/SignTransfer/SignTransferComponent.tsx
+++ src/iframe-entry/pages/SignTransfer/SignTransferComponent.tsx
@@ -67,7 +67,7 @@ export const SignTransfer: FC<Props> = ({
         onReject={onReject}
         onConfirm={onConfirm}
     >
-        <Flex px="$40" py="$30" bg="main.$900">
+        <Flex px="$40" py="$20" bg="main.$900">
             <Flex
                 alignItems="center"
                 justifyContent="center"
@@ -88,8 +88,14 @@ export const SignTransfer: FC<Props> = ({
             </Flex>
         </Flex>
 
-        <Tabs px="$40">
-            <TabsList borderBottom="1px solid" borderColor="main.$700" mb="$30">
+        <Tabs>
+            <TabsList
+                borderBottom="1px solid"
+                borderColor="main.$700"
+                bg="main.$900"
+                mb="$30"
+                px="$40"
+            >
                 <Tab mr="32px" pb="12px">
                     <Text variant="body1">Main</Text>
                 </Tab>
@@ -101,84 +107,74 @@ export const SignTransfer: FC<Props> = ({
                 </Tab>
             </TabsList>
 
-            <Flex
-                mb="$30"
-                flexDirection="column"
-                bg="main.$800"
-                borderTop="1px solid"
-                borderTopColor="basic.$1000"
-            >
-                <TabPanels>
-                    <TabPanel>
-                        <Flex
-                            flexDirection="column"
-                            bg="main.$800"
-                            borderTop="1px solid"
-                            borderTopColor="basic.$1000"
+            <TabPanels bg="main.$800" mb="$30" px="$40">
+                <TabPanel>
+                    <Flex
+                        flexDirection="column"
+                        bg="main.$800"
+                        borderTop="1px solid"
+                        borderTopColor="basic.$1000"
+                    >
+                        <Text variant="body2" color="basic.$500">
+                            Recipient
+                        </Text>
+                        <AddressAvatar
+                            address={recipientAddress}
+                            alias={recipientName}
+                            addressWithCopy={true}
+                            mt="$5"
+                        />
+
+                        {attachement ? (
+                            <>
+                                <Text
+                                    mt="$20"
+                                    variant="body2"
+                                    color="basic.$500"
+                                >
+                                    Attachment
+                                </Text>
+
+                                <Text
+                                    mt="$5"
+                                    p="15px"
+                                    variant="body2"
+                                    color="standard.$0"
+                                    bg="basic.$900"
+                                    borderRadius="$4"
+                                >
+                                    {attachement}
+                                </Text>
+                            </>
+                        ) : null}
+
+                        <Text mt="$20" variant="body2" color="basic.$500">
+                            Fee
+                        </Text>
+                        <Select
+                            isDisabled={meta.feeList.length === 0}
+                            placement="top"
+                            renderSelected={(open): ReactElement => (
+                                <Selected
+                                    selected={selectedFee}
+                                    opened={open}
+                                />
+                            )}
                         >
-                            <Text variant="body2" color="basic.$500">
-                                Recipient
-                            </Text>
-
-                            <AddressAvatar
-                                address={recipientAddress}
-                                alias={recipientName}
-                                addressWithCopy={true}
-                                mt="$5"
-                            />
-
-                            {attachement ? (
-                                <>
-                                    <Text
-                                        mt="$20"
-                                        variant="body2"
-                                        color="basic.$500"
-                                    >
-                                        Attachment
-                                    </Text>
-
-                                    <Text
-                                        mt="$5"
-                                        p="15px"
-                                        variant="body2"
-                                        color="standard.$0"
-                                        bg="basic.$900"
-                                        borderRadius="$4"
-                                    >
-                                        {attachement}
-                                    </Text>
-                                </>
-                            ) : null}
-
-                            <Text mt="$20" variant="body2" color="basic.$500">
-                                Fee
-                            </Text>
-                            <Select
-                                isDisabled={meta.feeList.length === 0}
-                                placement="top"
-                                renderSelected={(open): ReactElement => (
-                                    <Selected
-                                        selected={selectedFee}
-                                        opened={open}
-                                    />
-                                )}
-                            >
-                                <List
-                                    onSelect={onFeeSelect}
-                                    options={feeList}
-                                    css={{ bottom: 56 }}
-                                ></List>
-                            </Select>
-                        </Flex>
-                    </TabPanel>
-                    <TabPanel>
-                        <TransactionDetails tx={tx} />
-                    </TabPanel>
-                    <TabPanel>
-                        <TransactionJson tx={tx} />
-                    </TabPanel>
-                </TabPanels>
-            </Flex>
+                            <List
+                                onSelect={onFeeSelect}
+                                options={feeList}
+                            ></List>
+                        </Select>
+                    </Flex>
+                </TabPanel>
+                <TabPanel>
+                    <TransactionDetails tx={tx} />
+                </TabPanel>
+                <TabPanel>
+                    <TransactionJson tx={tx} />
+                </TabPanel>
+            </TabPanels>
         </Tabs>
     </Confirmation>
 );
